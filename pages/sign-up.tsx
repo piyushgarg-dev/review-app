46e2b46bcad0c2e3c9dfa6d7cea60ca63cc00f62
Hemant <hrsmart2150@gmail.com>
2023-08-16 13:05:15 +0530

    Updated zod validations message

--- pages/sign-up.tsx
+++ pages/sign-up.tsx
@@ -13,8 +13,8 @@ import Link from 'next/link';
 
 
 const formSchema = z.object({
-    email: z.string().min(1),
-    password: z.string().min(8),
+    email: z.string().email({ message: "Invalid email" }),
+    password: z.string().min(8, { message: "Password must be at least 8 characters long" }),
 });
 
 const SignUp: React.FC = () => {
