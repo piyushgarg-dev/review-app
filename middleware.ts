c1b9eca07c06fd08e15c6d4fb6d28ae08cc6b346
Piyush Garg <piyushgarg.dev@gmail.com>
2023-09-17 12:54:37 +0530

    Added Support for Custom Domain

---
+++ middleware.ts
@@ -0,0 +1,43 @@
+import { NextRequest, NextResponse } from 'next/server'
+export const config = {
+  matcher: [
+    /*
+     * Match all paths except for:
+     * 1. /api routes
+     * 2. /_next (Next.js internals)
+     * 3. /examples (inside /public)
+     * 4. all root files inside /public (e.g. /favicon.ico)
+     */
+    '/((?!api/|__sentry/|_next/|_static/|[\\w-]+\\.\\w+).*)',
+  ],
+}
+export function middleware(req: NextRequest) {
+  const url = req.nextUrl
+  const hostname = req.headers.get('host')
+  const path = url.pathname
+
+  if (!hostname) return
+
+  const currentHost =
+    process.env.NODE_ENV === 'production' && process.env.VERCEL === '1'
+      ? hostname.replace(`.${process.env.NEXT_PUBLIC_APP_PUBLIC_DOMAIN}`, '')
+      : hostname.replace(`.localhost:3000`, '')
+
+  if (
+    !currentHost ||
+    currentHost === process.env.NEXT_PUBLIC_APP_DOMAIN ||
+    currentHost === `www.${process.env.NEXT_PUBLIC_APP_DOMAIN}`
+  )
+    return
+
+  if (
+    currentHost &&
+    currentHost === process.env.NEXT_PUBLIC_APP_DASHBOARD_DOMAIN
+  ) {
+    return NextResponse.rewrite(new URL(`/__app${path}${url.search}`, req.url))
+  }
+
+  return NextResponse.rewrite(
+    new URL(`/_sites/${currentHost}${path}${url.search}`, req.url)
+  )
+}
--- pages/__app/dashboard/[projectSlug]/forms/edit/[formId].tsx
+++ pages/__app/dashboard/[projectSlug]/forms/edit/[formId].tsx
@@ -1,22 +1,18 @@
-import type { NextPage } from 'next'
+import type { GetServerSideProps, NextPage } from 'next'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { useMemo } from 'react'
 
 import FormEdit from '@/components/Dashboard/FormEdit'
 import { useFormById } from '@/hooks/query/form'
-import { useSelectedProject } from '@/hooks/query/project'
 
-const FormEditPage: NextPage = () => {
-  const router = useRouter()
-  const formId = useMemo(
-    () => router.query.formId as string,
-    [router.query.formId]
-  )
+interface PageProps {
+  formId: string
+  projectSlug: string
+}
 
+const FormEditPage: NextPage<PageProps> = ({ formId, projectSlug }) => {
   const { form: reviewForm, isLoading } = useFormById(formId)
-
-  const { project: selectedProject } = useSelectedProject()
 
   if (isLoading) return <p>Loading....</p>
 
@@ -25,7 +21,7 @@ const FormEditPage: NextPage = () => {
       <section className="flex h-full w-full max-w-xl flex-col px-8 py-8">
         <Link
           className="offset_ring flex w-fit justify-between rounded-md px-2 py-1 text-sm text-gray-500 hover:bg-gray-50 dark:text-gray-400 dark:hover:bg-gray-700"
-          href={`/dashboard/${selectedProject?.slug}/forms`}
+          href={`/dashboard/${projectSlug}/forms`}
         >
           <span className="pr-2">←</span>
           Forms
@@ -41,6 +37,16 @@ const FormEditPage: NextPage = () => {
       </section>
     </div>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const q = context.query
+  return {
+    props: {
+      formId: q.formId as string,
+      projectSlug: q.projectSlug as string,
+    },
+  }
 }
 
 export default FormEditPage
