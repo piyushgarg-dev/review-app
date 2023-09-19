1633cc57408675ebb08f30dd991327c9a3d54ef5
Hemant <hrsmart2150@gmail.com>
2023-09-19 22:10:20 +0530

    fixed some warnings

--- components/Dashboard/DashboardMobileSidebar/index.tsx
+++ components/Dashboard/DashboardMobileSidebar/index.tsx
@@ -14,10 +14,8 @@ const DashboardMobileSidebar: React.FC = () => {
 
   return (
     <Sheet>
-      <SheetTrigger>
-        <Button variant="ghost" size="icon" className="md:hidden">
-          <Menu />
-        </Button>
+      <SheetTrigger className="inline-flex h-10 w-10 items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors hover:bg-accent hover:text-accent-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 md:hidden">
+        <Menu />
       </SheetTrigger>
       <SheetContent side="left" className="p-0">
         <DashboardSidebar />
