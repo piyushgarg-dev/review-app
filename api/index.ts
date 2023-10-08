27fe7b92ea7789d337f0b77dcd7e51473732ec2a
NiKHiLkr23 <nikhilkumarofficial23@gmail.com>
2023-10-08 23:54:07 +0530

    feat:updated landing page theme && responsiveness

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,5 @@
 import { GraphQLClient } from 'graphql-request'
-
+import axios from 'axios'
 const API_URL = process.env.NEXT_PUBLIC_AWS_API_URL as string | null | undefined
 
 if (!API_URL)
@@ -14,3 +14,18 @@ export const graphqlClient = new GraphQLClient(`${API_URL}/graphql`, {
       : '',
   }),
 })
+
+export const getGithubStars = async () => {
+  try {
+    const { data } = await axios.get(
+      'https://api.github.com/repos/piyushgarg-dev/review-app'
+    )
+    return data.stargazers_count
+  } catch (error) {
+    // console.error('Error fetching GitHub stars:', error)
+    if (error instanceof Error) {
+      return new Response(error.message, { status: 500 })
+    }
+    return new Response('Internal Server Error', { status: 500 })
+  }
+}
