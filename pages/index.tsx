1c7f4f6f6a4363751c91b4118e7c42866b0b3587
Piyush Garg <gargpiyush195@gmail.com>
2023-10-08 19:27:44 +0530

    Revert "feat: Designed and added landing page"

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,32 +5,32 @@ import { useEffect } from 'react'
 
 import { Button } from '@/components/ui/button'
 import { useCurrentUser } from '@/hooks/query/user'
-import Navbar from '@/components/ui/navbar'
-import Main from '@/components/ui/main'
-import Footer from '@/components/ui/footer'
 
 const HomePage: NextPage = () => {
   const { user } = useCurrentUser()
   const router = useRouter()
 
-  // useEffect(() => {
-  //   if (!user) {
-  //     router.replace('/signin')
-  //   }
-  // }, [router, user])
+  useEffect(() => {
+    if (!user) {
+      router.replace('/signin')
+    }
+  }, [router, user])
 
   return (
     <div>
       <Head>
         <title>Review App</title>
-        <meta
-          name="Review App"
-          content="Getting customer reviews of their products by only a Link"
-        />
       </Head>
-      <Navbar />
-      <Main />
-      <Footer />
+      <main className="flex h-screen w-screen items-center justify-center">
+        <div className="space-y-6 text-center">
+          <h1 className="text-4xl">
+            Hello {user?.firstName} {user?.lastName} 👋🏻
+          </h1>
+          <Button onClick={() => router.push('/dashboard')}>
+            Go to Dashboard
+          </Button>
+        </div>
+      </main>
     </div>
   )
 }
