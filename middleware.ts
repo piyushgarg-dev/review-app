fcd77187ccc8ce8756c928a411bcce3bdf3503bb
Hari1122-code <harikrishnasai1122@gmail.com>
2023-10-08 15:44:43 +0530

    redirecting to Domain signin and signup page resolved

--- middleware.ts
+++ middleware.ts
@@ -18,6 +18,10 @@ export function middleware(req: NextRequest) {
 
   if (!hostname) return
 
+  if(hostname==`${process.env.NEXT_PUBLIC_APP_DOMAIN}` && (path=="/signin" || path=="/signup")){ 
+    return NextResponse.redirect(`http://app.${process.env.NEXT_PUBLIC_APP_DOMAIN}${path}`);
+  }
+  
   const currentHost =
     process.env.NODE_ENV === 'production' && process.env.VERCEL === '1'
       ? hostname.replace(`.${process.env.NEXT_PUBLIC_APP_PUBLIC_DOMAIN}`, '')
