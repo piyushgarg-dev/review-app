bce4b0cff1e13cbe49fd4af762d7d77dc2b90c54
Anandateertha <www.anandateertha@gmail.com>
2023-10-15 17:45:05 +0530

    Used useRef as requested to focus the input field and used lucide instead of svg

--- components/Dashboard/FormEdit.tsx
+++ components/Dashboard/FormEdit.tsx
@@ -5,9 +5,10 @@ import {
   Globe,
   ImagePlus,
   User,
+  Pencil
 } from 'lucide-react'
 import Image from 'next/image'
-import { useCallback, useState } from 'react'
+import { useCallback, useRef, useState } from 'react'
 import { HexColorPicker } from 'react-colorful'
 import { useForm } from 'react-hook-form'
 import toast from 'react-hot-toast'
@@ -68,6 +69,7 @@ export interface FormEditProps {
 const FormEdit: React.FC<FormEditProps> = ({ reviewForm, onStepChange }) => {
   const [isCtaEnabled, setIsCtaEnabled] = useState(false)
   const [logoUrl, setLogoUrl] = useState('')
+  const inputref = useRef<HTMLInputElement | null>(null);
 
   const { mutateAsync: updateFormAsync } = useUpdateForm()
 
@@ -130,22 +132,20 @@ const FormEdit: React.FC<FormEditProps> = ({ reviewForm, onStepChange }) => {
               <FormControl>
                 <div className="relative mt-4 w-fit">
                   <input
-                    id="nameInput"
+                    ref={inputref}
                     disabled={form.formState.isSubmitting}
                     className="w-[7.5rem] rounded bg-transparent px-1 py-1 text-xl font-bold outline-dashed outline-1 outline-transparent duration-100 hover:outline-gray-300 focus:outline-gray-300"
-                    {...field}
+                    value={field.value}
+                    onChange={field.onChange}
                   />
                     <Button
                     type='button'
                     style={{ backgroundColor: 'transparent' }}
-                    onClick={() => {
-                      const input = document.getElementById('nameInput');
-                      if (input) {
-                        input.focus();
-                      }
-                    }}
+                    onClick={() => 
+                      inputref?.current?.focus()
+                    }
                     >
-                    <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M11.8536 1.14645C11.6583 0.951184 11.3417 0.951184 11.1465 1.14645L3.71455 8.57836C3.62459 8.66832 3.55263 8.77461 3.50251 8.89155L2.04044 12.303C1.9599 12.491 2.00189 12.709 2.14646 12.8536C2.29103 12.9981 2.50905 13.0401 2.69697 12.9596L6.10847 11.4975C6.2254 11.4474 6.3317 11.3754 6.42166 11.2855L13.8536 3.85355C14.0488 3.65829 14.0488 3.34171 13.8536 3.14645L11.8536 1.14645ZM4.42166 9.28547L11.5 2.20711L12.7929 3.5L5.71455 10.5784L4.21924 11.2192L3.78081 10.7808L4.42166 9.28547Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg>
+                    <Pencil size={15}/>
                     </Button>
                 </div>
               </FormControl>
