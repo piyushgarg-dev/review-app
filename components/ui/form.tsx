44b164548b4c7c289f2eb7fe2bed198d8d54c063
Niranjan-Baur <73574249+Niranjan-Baur@users.noreply.github.com>
2023-10-18 21:00:14 +0530

    fix: Error msg and label color fix

--- components/ui/form.tsx
+++ components/ui/form.tsx
@@ -93,7 +93,7 @@ const FormLabel = React.forwardRef<
   return (
     <Label
       ref={ref}
-      className={cn(error && 'text-destructive', className)}
+      className={cn(error && 'text-destructive text-white', className)}
       htmlFor={formItemId}
       {...props}
     />
@@ -155,7 +155,7 @@ const FormMessage = React.forwardRef<
     <p
       ref={ref}
       id={formMessageId}
-      className={cn('text-sm font-medium text-destructive', className)}
+      className={cn('text-sm font-medium text-destructive text-red-600', className)}
       {...props}
     >
       {body}
