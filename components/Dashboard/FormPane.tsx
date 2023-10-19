d52d3ef218eee54a86c934628d8f57f19e1a03c1
SwastikKasera <swastikkasera@gmail.com>
2023-10-19 08:30:15 +0530

    "Space Added"

--- components/Dashboard/FormPane.tsx
+++ components/Dashboard/FormPane.tsx
@@ -2,9 +2,9 @@ import { Copy, Pencil, Share2, Trash2 } from 'lucide-react'
 import Image from 'next/image'
 import toast from 'react-hot-toast'
 import Link from 'next/link'
-import toast from 'react-hot-toast'
 import { useRouter } from 'next/router'
 import { useState } from 'react'
+
 import { Checkbox } from '@/components/ui/checkbox'
 import {
   Tooltip,
