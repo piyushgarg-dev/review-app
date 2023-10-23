d50bb4d0eace0e125fc1f5a6c59e56d3fec9402a
Chaitanya Shahare <shaharechaitanya3@gmail.com>
2023-10-23 12:31:36 +0530

    fix: increase padding in 404 page
    there was no padding for left and right on the custom 404 page, so added some padding for the entire 404 page component

--- pages/[...slug].tsx
+++ pages/[...slug].tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 const Custom404 = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
+    <div className="flex flex-col items-center justify-center h-screen p-6">
       <h1 className="text-3xl md:text-4xl font-semibold mb-4">404 - Page Not Found</h1>
       <p className="text-lg md:text-xl whitespace-wrap text-gray-600 mb-6">We apologize for the inconvenience. The page you are looking for does not exist or is under development.</p>
       <Link className="flex items-center text-blue-500 hover:text-blue-700" href="/dashboard">
